4f309a0eb6f5edfae00f7c4e860c5b515b319514
Tibor Leupold <tibor@lpld.io>
2019-09-19 18:37:07 -0700

    Fixed typo

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -13,6 +13,6 @@ CREATE TABLE post (
 	created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	title TEXT NOT NULL,
 	body TEXT NOT NULL,
-	FORGEIN KEY (author_id) REFERENCES user (id)
+	FOREIGN KEY (author_id) REFERENCES user (id)
 );
 
