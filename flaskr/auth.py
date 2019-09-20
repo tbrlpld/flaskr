59078042cbca02c17336c5a9eee7342f12f1114c
Tibor Leupold <tibor@lpld.io>
2019-09-19 21:00:07 -0700

    Created working blog functionality

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -87,9 +87,11 @@ def logout():
 
 
 def login_required(view):
-    @functools.warps(view)
+    @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
-            return redirect(url_for("auth.login"))
+            return redirect(url_for('auth.login'))
 
         return view(**kwargs)
+
+    return wrapped_view
