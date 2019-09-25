e3535aa79d3aa716ca2d2d3b410c8070cdbbb7a0
Tibor Leupold <tibor@lpld.io>
2019-09-25 12:31:34 -0700

    Added function to retrieve comments to post

--- flaskr/comments.py
+++ flaskr/comments.py
@@ -10,6 +10,21 @@ from flaskr.db import get_db
 bp = Blueprint("comments", __name__, url_prefix="/comments")
 
 
+def get_comments_for_post(post_id=None):
+    """
+    Get all comments for a post with given id.
+    """
+    if post_id is not None:
+        db = get_db()
+        comments = db.execute(
+            "SELECT c.id, body, created, post_id, author_id, username"
+            " FROM comment c JOIN user u ON c.author_id = u.id"
+            " WHERE post_id = ?",
+            (post_id,)
+        ).fetchall()
+        return comments
+
+
 @bp.route("/create", methods=("POST",))
 @login_required
 def create():
--- tests/test_comments.py
+++ tests/test_comments.py
@@ -1,3 +1,4 @@
+from flaskr.comments import get_comments_for_post
 from flaskr.db import get_db
 
 
@@ -21,3 +22,11 @@ def test_create(client, auth, app):
         # New there should be two comments in the db
         count = db.execute("SELECT COUNT(id) FROM comment").fetchone()[0]
         assert count == 2
+
+
+def test_get_comments(app):
+    with app.app_context():
+        comments = get_comments_for_post(post_id=1)
+        assert len(comments) == 1
+        assert "test comment body" in comments[0]["body"]
+        assert comments[0]["username"] == "other"
