03913151455a54c5786683093d0a9f7b393a9af3
Tibor Leupold <tibor@lpld.io>
2019-09-25 17:08:10 -0700

    Showing like button to logged in users

--- tests/test_likes.py
+++ tests/test_likes.py
@@ -28,3 +28,21 @@ def test_no_double_like(client, auth, app):
 
     response = client.post("/likes/create", data={"post_id": 1})
     assert response.status_code == 403  #  Forbidden
+
+
+def test_logged_in_user_sees_like_link(client, auth, app):
+    # No link when not logged in
+    response = client.get("/1/detail")
+    assert b"/likes/create" not in response.data
+
+    with app.app_context():
+        db = get_db()
+        # Making sure that there is no like by the user for the post
+        like_count = db.execute(
+            "SELECT COUNT(post_id) FROM like WHERE post_id = 1 AND user_id = 1"
+        ).fetchone()[0]
+        assert like_count == 0
+
+    auth.login()
+    response = client.get("/1/detail")
+    assert b"/likes/create" in response.data
