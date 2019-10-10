e6b58d4064f0b1f510dfbc3652cc6439c46e1308
Tibor Leupold <tibor@lpld.io>
2019-10-10 12:03:14 -1000

    Added route for title filtered index

--- flaskr/search.py
+++ flaskr/search.py
@@ -8,4 +8,18 @@ bp = Blueprint("search", __name__, url_prefix="/search")
 
 @bp.route("<string:title>")
 def display_search_filtered_index(title):
-    return render_template("blog/index.html")
+    db = get_db()
+    posts = db.execute(
+        "SELECT p.id, p.title, p.body, p.created, p.author_id, u.username,"
+        " GROUP_CONCAT(t.name, ' ') AS tag_string"
+        " FROM post p"
+        " JOIN user u ON p.author_id = u.id"
+        # LEFT JOIN makes the existence of values in the right table optional!
+        " LEFT JOIN post_tag pt ON p.id = pt.post_id"
+        " LEFT JOIN tag t ON pt.tag_id = t.id"
+        " WHERE p.title = ?"
+        " GROUP BY p.id"
+        " ORDER BY p.created DESC",
+        (title,)
+    ).fetchall()
+    return render_template("blog/index.html", posts=posts)
