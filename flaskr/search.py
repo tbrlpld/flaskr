f23a17742e65b0056da286332231fd2c083e1269
Tibor Leupold <tibor@lpld.io>
2019-10-10 12:09:32 -1000

    Search term contained in title instead of exact match

--- flaskr/search.py
+++ flaskr/search.py
@@ -17,9 +17,9 @@ def display_search_filtered_index(title):
         # LEFT JOIN makes the existence of values in the right table optional!
         " LEFT JOIN post_tag pt ON p.id = pt.post_id"
         " LEFT JOIN tag t ON pt.tag_id = t.id"
-        " WHERE p.title = ?"
+        " WHERE p.title LIKE ?"
         " GROUP BY p.id"
         " ORDER BY p.created DESC",
-        (title,)
+        ("%" + title + "%",)
     ).fetchall()
     return render_template("blog/index.html", posts=posts)
--- tests/test_search.py
+++ tests/test_search.py
@@ -1,15 +1,16 @@
 from flask import url_for
 
 from flaskr.blog import create_post
-from flaskr.db import get_db
 
 
 def test_filtered_index_by_url(app, client):
     with app.test_request_context():
         create_post("other title", "other body", 2)
         response = client.get(
-            url_for("search.display_search_filtered_index", title="test title")
+            url_for("search.display_search_filtered_index", title="test")
         )
         assert response.status_code == 200
         assert b"test title" in response.data
         assert b"other title" not in response.data
+
+
