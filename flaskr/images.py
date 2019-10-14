49d4a4a096b4b8957de4376a89a9dc8d2cd9ef01
Tibor Leupold <tibor@lpld.io>
2019-10-13 15:11:17 -1000

    Added function to save image to upload dir

--- flaskr/images.py
+++ flaskr/images.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Blueprint, send_from_directory, current_app
 
 
@@ -8,3 +10,9 @@ bp = Blueprint("images", __name__, url_prefix="/images")
 def get(filename):
     return send_from_directory(current_app.config['UPLOAD_DIR'],
                                filename, as_attachment=False)
+
+
+def save_image_to_upload_dir(content, filename):
+    save_image_path = os.path.join(current_app.config["UPLOAD_DIR"], filename)
+    with open(save_image_path, "wb") as f:
+        f.write(content)
--- tests/test_images.py
+++ tests/test_images.py
@@ -4,29 +4,56 @@ import shutil
 import pytest
 from flask import url_for
 
+from flaskr import images
+
+
+class ExampleImage(object):
+    def __init__(self):
+        self.filename = "example.png"
+        self.path = os.path.join(
+            os.path.dirname(__file__), self.filename)
+        with open(self.path, mode="rb") as f:
+            self.content = f.read()
+
 
 @pytest.fixture
 def example_image_to_upload_dir(app):
-    example_image_filename = "example.png"
-    example_image_path = os.path.join(
-        os.path.dirname(__file__), example_image_filename)
-    shutil.copyfile(example_image_path,
+    example_image = ExampleImage()
+    shutil.copyfile(example_image.path,
                     os.path.join(app.config["UPLOAD_DIR"],
-                                 example_image_filename))
+                                 example_image.filename))
 
 
 def test_get_example_image_from_url(app, client, example_image_to_upload_dir):
+    example_image = ExampleImage()
     with app.app_context():
-        example_image_filename = "example.png"
-        assert example_image_filename in os.listdir(app.config["UPLOAD_DIR"])
-        example_image_path = os.path.join(app.config["UPLOAD_DIR"],
-                                          example_image_filename)
-        with open(example_image_path, mode="rb") as f:
-            example_image_content = f.read()
+        assert example_image.filename in os.listdir(app.config["UPLOAD_DIR"])
 
     with app.test_request_context():
         response = client.get(
-            url_for("images.get", filename=example_image_filename))
+            url_for("images.get", filename=example_image.filename))
         assert response.status_code == 200
         assert response.mimetype == "image/png"
-        assert response.data == example_image_content
+        assert response.data == example_image.content
+
+
+def test_save_image_to_upload_dir(app):
+    example_image = ExampleImage()
+    with app.app_context():
+        # File does not exist
+        assert (example_image.filename
+                not in os.listdir(app.config["UPLOAD_DIR"]))
+
+        # Saving file
+        images.save_image_to_upload_dir(content=example_image.content,
+                                    filename=example_image.filename)
+
+        # File exists
+        assert (example_image.filename
+                in os.listdir(app.config["UPLOAD_DIR"]))
+        saved_image_path = os.path.join(app.config["UPLOAD_DIR"],
+                                        example_image.filename)
+        # File has correct content
+        with open(saved_image_path, mode="rb") as f:
+            saved_image_content = f.read()
+        assert saved_image_content == example_image.content
