7f83fe83d4eab317e88617c45155c97f961a98a5
Tibor Leupold <tibor@lpld.io>
2019-10-14 12:02:23 -1000

    Changed image saving function
    The function now utilizes the object type in which the file will be
    available in the request object. The image is saved with a random
    filename (based on UUID), but the same extension as the original
    filename on the client.

--- flaskr/images.py
+++ flaskr/images.py
@@ -1,4 +1,5 @@
 import os
+import uuid
 
 from flask import Blueprint, send_from_directory, current_app
 
@@ -12,7 +13,33 @@ def get(filename):
                                filename, as_attachment=False)
 
 
-def save_image_to_upload_dir(content, filename):
-    save_image_path = os.path.join(current_app.config["UPLOAD_DIR"], filename)
-    with open(save_image_path, "wb") as f:
-        f.write(content)
+def get_random_string():
+    """Helper function returning a random uuid without the dashes"""
+    return uuid.uuid4().hex
+
+
+def save_image_to_upload_dir(filestrorage_obj, _filename=None):
+    """
+    Save the filestorage object as a file in the upload directory
+
+    The file is saved with a random filename. The private `_filename` argument
+    can be used during testing to force the file being saved with a defined
+    filename.
+
+    The random filename will have the same fileextension as the file extension
+    on the client. The random filename is based on UUID which should make
+    name clashes basically impossible.
+
+    :param filestorage_obj: FileStorage object that is attached to the request
+                            when uploaded.
+    :type filestrorage_obj: werkzeug.datastructures.FileStorage
+
+    :param _filename: Private argument for testing to override the randomly
+                      generated filename
+    :type filename: string
+    """
+    _, extension = os.path.splitext(filestrorage_obj.filename)
+    filename = _filename or (get_random_string() + extension)
+    save_image_path = os.path.join(
+        current_app.config["UPLOAD_DIR"], filename)
+    filestrorage_obj.save(dst=save_image_path)
--- tests/test_images.py
+++ tests/test_images.py
@@ -3,6 +3,7 @@ import shutil
 
 import pytest
 from flask import url_for
+from werkzeug.datastructures import FileStorage
 
 from flaskr import images
 
@@ -12,6 +13,7 @@ class ExampleImage(object):
         self.filename = "example.png"
         self.path = os.path.join(
             os.path.dirname(__file__), self.filename)
+        self.fileobject = open(self.path, mode="rb")
         with open(self.path, mode="rb") as f:
             self.content = f.read()
 
@@ -37,16 +39,30 @@ def test_get_example_image_from_url(app, client, example_image_to_upload_dir):
         assert response.data == example_image.content
 
 
-def test_save_image_to_upload_dir(app):
+def test_get_random_string():
+    first_string = images.get_random_string()
+    second_string = images.get_random_string()
+    assert first_string != second_string
+    assert "-" not in second_string
+    assert "." not in second_string
+
+
+def test_save_image_to_upload_dir_with_given_filename(app):
     example_image = ExampleImage()
+    # The werkzeug filestorage type is how that uploaded image will be attached
+    # to the request by Flask. The FileStorage object already comes with a save
+    # method. It makes sense to utilize that.
+    filestorage = FileStorage(
+        stream=example_image.fileobject,
+        filename=example_image.filename)
     with app.app_context():
         # File does not exist
         assert (example_image.filename
                 not in os.listdir(app.config["UPLOAD_DIR"]))
 
         # Saving file
-        images.save_image_to_upload_dir(content=example_image.content,
-                                    filename=example_image.filename)
+        images.save_image_to_upload_dir(filestrorage_obj=filestorage,
+                                        _filename=example_image.filename)
 
         # File exists
         assert (example_image.filename
@@ -54,6 +70,38 @@ def test_save_image_to_upload_dir(app):
         saved_image_path = os.path.join(app.config["UPLOAD_DIR"],
                                         example_image.filename)
         # File has correct content
+        with open(saved_image_path, mode="rb") as f:
+            saved_image_content = f.read()
+        assert saved_image_content == example_image.content
+
+
+def test_save_image_to_upload_dir_without_given_filename(app):
+    example_image = ExampleImage()
+    filestorage = FileStorage(
+        stream=example_image.fileobject,
+        filename=example_image.filename)
+    with app.app_context():
+        # File does not exist
+        uploaded_files = os.listdir(app.config["UPLOAD_DIR"])
+        assert (example_image.filename
+                not in uploaded_files)
+        assert len(uploaded_files) == 0
+
+        # Saving file
+        images.save_image_to_upload_dir(filestrorage_obj=filestorage)
+
+        uploaded_files = os.listdir(app.config["UPLOAD_DIR"])
+        assert len(uploaded_files) == 1
+        # The saved file does not have the example filename!
+        assert (example_image.filename
+                not in uploaded_files)
+
+        saved_filename = uploaded_files[0]
+        _, saved_fileextension = os.path.splitext(saved_filename)
+        assert saved_fileextension == ".png"
+        # But the content is correct
+        saved_image_path = os.path.join(app.config["UPLOAD_DIR"],
+                                        saved_filename)
         with open(saved_image_path, mode="rb") as f:
             saved_image_content = f.read()
         assert saved_image_content == example_image.content
